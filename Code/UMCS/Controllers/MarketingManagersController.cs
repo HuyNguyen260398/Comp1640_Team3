6aa219f5b02d3f9ef2ec62b81f02af292d972bc8
Huy Nguyen <huynguyen260398@gmail.com>
2019-04-24 21:24:28 +0700

    MM Controller

--- Code/UMCS/Controllers/MarketingManagersController.cs
+++ Code/UMCS/Controllers/MarketingManagersController.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UMCS.Models;
 
 namespace UMCS.Controllers
 {
     public class MarketingManagersController : Controller
     {
+        UMCSEntities db = new UMCSEntities();
+
         // GET: MarketingManagers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult FacultiesList()
+        {
+            var falcultieslist = db.Faculties.ToList();
+            return View(falcultieslist);
+        }
+
+        public ActionResult ContributionsList(string id)
+        {
+            int f_id = Convert.ToInt32(id);
+            var contributesList = db.Contributions.Where(c => c.Student.FacultiesID == f_id);
+            var selectedContributes = contributesList.Where(s => s.Status == "Selected" || s.Status == "Commented").ToList();
+            return View(selectedContributes);
         }
     }
 }
