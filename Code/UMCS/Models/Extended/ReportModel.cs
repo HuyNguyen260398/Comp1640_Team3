9ec0c260e71d167ec367df557d5d1ed9ecec9504
Nguyễn Thành Công <congntgcs16023@fpt.edu.vn>
2019-04-26 02:34:58 +0700

    Update database

---
+++ Code/UMCS/Models/Extended/ReportModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UMCS.Models.Extended
+{
+    public class ReportModel
+    {
+        public int FacultyId { get; set; }
+        public string FacultyName { get; set; }
+        public int NumberOfContribution { get; set; }
+        public float PercentOfContribution { get; set; }
+        public int NumberOfContributor { get; set; }
+    }
+}
