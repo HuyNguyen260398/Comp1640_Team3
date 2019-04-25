9de43465036318cac3e51d0dc135d5c2f5a4b271
Huy Nguyen <huynguyen260398@gmail.com>
2019-04-25 11:11:40 +0700

    download zip

--- Code/UMCS/Controllers/MarketingManagersController.cs
+++ Code/UMCS/Controllers/MarketingManagersController.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Ionic.Zip;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using UMCS.Models;
+using UMCS.Models.Extended;
 
 namespace UMCS.Controllers
 {
@@ -28,7 +31,47 @@ namespace UMCS.Controllers
             int f_id = Convert.ToInt32(id);
             var contributesList = db.Contributions.Where(c => c.Student.FacultiesID == f_id);
             var selectedContributes = contributesList.Where(s => s.Status == "Selected" || s.Status == "Commented").ToList();
+            ViewBag.FID = f_id;
             return View(selectedContributes);
+        }
+
+        public FileResult DownloadZip(string id)
+        {
+            int f_id = Convert.ToInt32(id);
+            var contributesList = db.Contributions.Where(c => c.Student.FacultiesID == f_id);
+            var selectedContributes = contributesList.Where(s => s.Status == "Selected" || s.Status == "Commented").ToList();
+
+            //List<string> filePaths = new List<string>();
+            //foreach(var item in selectedContributes)
+            //{
+            //    filePaths.Add(item.ArchiveLink);
+            //}
+            
+            List<FileModel> files = new List<FileModel>();
+            foreach (var item in selectedContributes)
+            {
+                files.Add(new FileModel()
+                {
+                    FileName = item.Title,
+                    FilePath = item.ArchiveLink
+                });
+            }
+
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.AlternateEncodingUsage = ZipOption.AsNecessary;
+                zip.AddDirectoryByName("Files");
+                foreach (FileModel file in files)
+                {
+                    zip.AddFile(file.FilePath, "Files");
+                }
+                string zipName = String.Format("Zip_{0}.zip", DateTime.Now.ToString("yyyy-MMM-dd-HHmmss"));
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    zip.Save(memoryStream);
+                    return File(memoryStream.ToArray(), "application/zip", zipName);
+                }
+            }
         }
     }
 }
---
+++ Code/UMCS/Models/Extended/FileModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UMCS.Models.Extended
+{
+    public class FileModel
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
